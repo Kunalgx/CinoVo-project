e484fd5c923ba8492338dac1c1cfa07dea2c26ef
Kunalgx <Kunalkumar94937@gmail.com>
2026-09-08 19:58:30 +0530

    Add server direct check

--- CineVo/server/src/server.js
+++ CineVo/server/src/server.js
@@ -4,6 +4,12 @@ import { connectDB } from "./config/db.js";
 import { seedAudioAvailability } from "./data/audioAvailability.seed.js";
 
 const PORT = process.env.PORT || 5000;
+app.get("/server-direct-check", (req, res) => {
+  res.json({
+    success: true,
+    message: "server.js is receiving requests",
+  });
+});
 
 try {
   await connectDB();
