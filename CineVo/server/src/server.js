f67431e8f05c7d6c75b46a06fb77e2df5c862afa
Kunalgx <Kunalkumar94937@gmail.com>
2026-09-08 20:18:57 +0530

    "Remove temporary route diagnostic"

--- CineVo/server/src/server.js
+++ CineVo/server/src/server.js
@@ -4,12 +4,7 @@ import { connectDB } from "./config/db.js";
 import { seedAudioAvailability } from "./data/audioAvailability.seed.js";
 
 const PORT = process.env.PORT || 5000;
-console.log(
-  "REGISTERED ROUTES:",
-  app.router?.stack
-    ?.filter((layer) => layer.route)
-    .map((layer) => Object.keys(layer.route.methods)[0].toUpperCase() + " " + layer.route.path)
-);
+
 
 try {
   await connectDB();
